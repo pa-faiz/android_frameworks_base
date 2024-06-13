c80cb90dfe12eff25ebd6268c9ec7eedafa9107c
Dhina17 <dhinalogu@gmail.com>
2024-06-13 13:15:27 +0530

    SystemUI: Network Traffic [1/2]
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Sat Jan 13 23:26:48 2018 -0800
    
        SystemUI: Network Traffic [1/3]
    
        *) Add lineage-sdk internal NetworkTraffic to the statusbar
           using the LineageStatusBarItemHolder helper.
    
        Change-Id: Ib947832860970a3bccfac70d27a4761f6164d3d5
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   Thu May 6 13:50:05 2021 +0200
    
        SystemUI: Force left-to-right for network traffic monitor holder
    
        Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/3445
        Change-Id: I19ea852cedfd2a69a47006977f1b74a172008058
    
    Author: Wolfram Liebchen <achalmgucker@googlemail.com>
    Date:   Mon Jan 23 22:03:20 2023 +0100
    
        SystemUI: Network traffic with center cutout [1/3]
    
        Add possible network traffic display locations on the left
        and right side.
    
        Change-Id: Ic209aa1c0816cb22a5cf4ad9f19908746d38fc68
    
    Change-Id: Ib947832860970a3bccfac70d27a4761f6164d3d5
    
    SystemUI: Network Traffic [1.5/2]
    
    Change-Id: I793d01f1c1de3413e11f7d78c2526977fd8175a4
    
    core: Add LineageStatusBarItem interfaces
    
    *) Introduce LineageStatusBarItem class that contains interfaces
       and utility methods that can be used by status bar items that
       that are hosted here in the lineage-sdk in order to communicate
       with systemui.
    
    *) Nested interface: Manager
       Implemented by layout view LineageStatusBarItemHolder in systemui.
       Contains methods that are used to request receipt of certain
       systemui events.
    
    *) Nested interface: DarkReceiver
       Can be used to listen for systemui DarkReceiver events.
    
    *) Nested interface: VisibilityReceiver
       Can be used to listen for changes in visibility of a statusbar item.
       Works even if the statusbar item sets it's own visibility to GONE.
    
    *) Util method: findManager(View v) walks up through parent views to
       look for something that implements Manager.
    
    *) Systemui side that provides a layout that implements Manager:
       https://review.lineageos.org/#/c/202265/
    
    [mikeNG : Update LineageStatusBarItem for T ]
    
    Change-Id: Ie237db5bfac9994b87de39372505cd61c39d89ee
    
    Network traffic with automatic units
    
    A new network traffic display unit option "automatic" offers
    a compact display of the network traffic by using at maximum
    three digits and an abbreviated unit string.
    Comes in handy for situations with reduced space in the
    status bar.
    
    Change-Id: Ib4d969924ad5a345b03540070e49a0473f343ad3
    
    core: Fix network traffic settings in secondary users
    
    Change-Id: I936c6cee3076c40c30627578b1e2b78b22acdd39
    
    Lineage: Move out UserContentObserver from services
    
    So it can be used everywhere.
    
    Change-Id: I6dec5503ecd7a21bf2b3145ca0c132fa383a888d
    
    SystemUI: Add Lineage statusbar item holder
    
    *) To be used as a layout to hold one or more lineage-sdk status bar
       feature classes.
    
    *) Propagates visibility and tint information via interfaces
       defined in lineage-sdk internal.
    
    *) Supports adding further interfaces without impacting existing use
       cases.
    
    Change-Id: I4d4361fcb682c1940133bee1029b8b759f93e05f
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

--- core/java/com/android/internal/database/UserContentObserver.java
+++ core/java/com/android/internal/database/UserContentObserver.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License
  */
-package com.android.server.lineage.common;
+package com.android.internal.database;
 
 import android.app.ActivityManagerNative;
 import android.app.IUserSwitchObserver;
